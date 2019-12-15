9448c89e211cf0e1d96e17f154a7b4b6ab227c74
Nenad Nikic <nesani@ymail.com>
2019-12-15 15:36:53 +0100

    Try-ed all Return types for ActionResult.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,10 +12,13 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-
             var movie = new Movie() { Name = "Shrek" };
 
             return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy ="name" });
         }
     }
 }
