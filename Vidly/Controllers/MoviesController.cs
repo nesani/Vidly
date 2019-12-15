709cf50a997eb524c4b960fc63a446389209a999
Nenad Nikic <nesani@ymail.com>
2019-12-15 15:48:49 +0100

    Added new action, added default routing on controller.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -20,5 +20,26 @@ namespace Vidly.Controllers
             //return new EmptyResult();
             //return RedirectToAction("Index", "Home", new {page = 1, sortBy ="name" });
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if(string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(string.Format($"pageIndex={pageIndex}&sortBy={sortBy}"));
+        }
     }
 }
