70eafae15f9ba106d2a727180f40e8ce49e5e755
Nenad Nikic <nesani@ymail.com>
2020-01-19 19:38:33 +0100

    Started WebApi tutorials.

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+
+        private ApplicationDbContext dbContext;
+
+        public CustomersController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
+        //Get /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return dbContext.Customers.ToList();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            var customer = dbContext.Customers.SingleOrDefault(i => i.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        //POST /Api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            dbContext.Customers.Add(customer);
+            dbContext.SaveChanges();
+
+            return customer;
+        }
+
+        //PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = dbContext.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDb.Name = customer.Name;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            customerInDb.MembershipType = customer.MembershipType;
+            customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerInDb.Birthdate = customer.Birthdate;
+
+            dbContext.SaveChanges();
+
+
+        }
+
+        //DELETE api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = dbContext.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            dbContext.Customers.Remove(customerInDb);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Vidly/DTO/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTO
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name")]
+        public string Name { get; set; }
+
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18yearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
